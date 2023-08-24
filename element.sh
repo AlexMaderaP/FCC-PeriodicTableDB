2318b2391c1a06d4225d505b5cb825c97a5c77d1
AlexMaderaP <alejandro.maderap@gmail.com>
2023-08-24 14:22:24 -0400

    feat: display message with element data

--- element.sh
+++ element.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only --no-align -c"
 
 if [[ -z $1 ]]
 then
@@ -10,19 +10,21 @@ else
   then
     #get element by atomic number
     ELEMENT_BY_ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
-
+    ELEMENT_DATA=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN elements using(atomic_number) WHERE atomic_number='$ELEMENT_BY_ATOMIC_NUMBER'")
   # if not a number
   else  
 
     # get element by symbol
     ELEMENT_BY_SYMBOL=$($PSQL "SELECT symbol from elements WHERE symbol='$1'")
-    
+    ELEMENT_DATA=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN elements using(atomic_number) WHERE symbol='$ELEMENT_BY_SYMBOL'")
+
     # if not found
     if [[ -z $ELEMENT_BY_SYMBOL ]]
     then
       # get element by name
       ELEMENT_BY_NAME=$($PSQL "SELECT name from elements WHERE name='$1'")
-      
+      ELEMENT_DATA=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties INNER JOIN elements using(atomic_number) WHERE name='$ELEMENT_BY_NAME'")
+
       # if not found
       if [[ -z $ELEMENT_BY_NAME ]]
       then
@@ -32,4 +34,12 @@ else
       
     fi
   fi
+
+  if [[ -n $ELEMENT_DATA ]]
+  then
+  echo "$ELEMENT_DATA" | sed 's/|/ /g' | (read NUM NAME SYMBOL TYPE MASS MELT_POINT BOIL_POINT; 
+  echo "The element with atomic number $NUM is $NAME ($SYMBOL). It's a nonmetal, with a mass of $MASS amu. $NAME has a melting point of $MELT_POINT celsius and a boiling point of $BOIL_POINT celsius." )
+  fi
+  
+  
 fi
